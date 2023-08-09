a58e4ea23ec99ddfc9cd56bb0117e9f747cb2741
tomasccanavilhas@gmail.com <tomasccanavilhas@gmail.com>
2023-08-09 03:52:25 +0100

    Updates

---
+++ precache-manifest.de421028ad89872d96472bbc7412d8e4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "77c47c027714fcae1477",
+    "url": "/static/css/main.1aa86a57.chunk.css"
+  },
+  {
+    "revision": "77c47c027714fcae1477",
+    "url": "/static/js/main.77c47c02.chunk.js"
+  },
+  {
+    "revision": "6c70623b6b27c6c396a4",
+    "url": "/static/css/1.b6d67028.chunk.css"
+  },
+  {
+    "revision": "6c70623b6b27c6c396a4",
+    "url": "/static/js/1.6c70623b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c012b708c736b1148b1f6b44feb1e8f",
+    "url": "/static/media/CV_Tomas_Canavilhas.8c012b70.pdf"
+  },
+  {
+    "revision": "ce32586193390a01e7ad75f114aa02ce",
+    "url": "/static/media/avatar.ce325861.jpeg"
+  },
+  {
+    "revision": "20206738b2bffb741b00200d5d3d6d20",
+    "url": "/static/media/fa-regular-400.20206738.ttf"
+  },
+  {
+    "revision": "8b0ddedbb27cbc9971c8667caa8a0cc1",
+    "url": "/static/media/fa-brands-400.8b0ddedb.woff2"
+  },
+  {
+    "revision": "61f30b79daf5b31f0d254a31fba66158",
+    "url": "/static/media/fa-regular-400.61f30b79.woff2"
+  },
+  {
+    "revision": "4bc58bc16bb05a05d3a47a4f7e143b75",
+    "url": "/static/media/fa-v4compatibility.4bc58bc1.woff2"
+  },
+  {
+    "revision": "0ab3921d9b80975c5597432ab59f5d0a",
+    "url": "/static/media/fa-brands-400.0ab3921d.ttf"
+  },
+  {
+    "revision": "e2ceb83946c9e5fc7eab24453a03bffb",
+    "url": "/static/media/fa-solid-900.e2ceb839.ttf"
+  },
+  {
+    "revision": "d20cedd7e254d4b58b721b6995ca52b4",
+    "url": "/static/media/fa-v4compatibility.d20cedd7.ttf"
+  },
+  {
+    "revision": "c64278386c2bbb5e293e11b94ca2f6d1",
+    "url": "/static/media/fa-solid-900.c6427838.woff2"
+  },
+  {
+    "revision": "6ca31485b7ba4db11d8ad8727581f859",
+    "url": "/static/media/sun.6ca31485.svg"
+  },
+  {
+    "revision": "8bdf7e4d0b36db42b353dd247f5df0f7",
+    "url": "/static/media/linkedin.8bdf7e4d.svg"
+  },
+  {
+    "revision": "6b04dc06356a3c81096ff32dc820df89",
+    "url": "/static/media/twitter.6b04dc06.svg"
+  },
+  {
+    "revision": "13a1bb4dfb579c91b9718001a825545a",
+    "url": "/static/media/github.13a1bb4d.svg"
+  },
+  {
+    "revision": "ce479a83110a3035ac4ddd9d92889a38",
+    "url": "/index.html"
+  }
+];
